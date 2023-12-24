b0eac020dfb9db84e790e0f445f2e25236863d5f
sayeemur <mursalin.eee@gmail.com>
2023-12-24 16:59:29 +0100

    dummy data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE Todos (
     dueDate DATETIME,
     priority INT
 );
+
+INSERT INTO Todos (description, dueDate, priority) VALUES
+('Finish homework', '2019-01-01 12:00:00', 1),
+('Go to the gym', '2019-01-01 13:00:00', 2),
+('Go to the supermarket', '2019-01-01 14:00:00', 3),
+('Go to the cinema', '2019-01-01 15:00:00', 4),
+('Go to the theatre', '2019-01-01 16:00:00', 5);
