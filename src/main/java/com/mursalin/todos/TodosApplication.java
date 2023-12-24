096372bcbb7f2016aa85680fc5d960956e9865e5
sayeemur <mursalin.eee@gmail.com>
2023-12-24 15:45:55 +0100

    initial commit

---
+++ src/main/java/com/mursalin/todos/TodosApplication.java
@@ -0,0 +1,14 @@
+package com.mursalin.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosApplication.class, args);
+	}
+
+}
