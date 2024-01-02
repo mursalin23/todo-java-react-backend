62996a0748de9f4ddcc148d8299005a4457c5afb
sayeemur <mursalin.eee@gmail.com>
2024-01-02 09:05:44 +0100

    seed data only needed at the start

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,9 +5,11 @@ CREATE TABLE IF NOT EXISTS Todo (
     priority INTEGER
 );
 
+/**
 INSERT INTO Todo (description, dueDate, priority) VALUES
 ('Finish homework', '2019-01-01 12:00:00', 1),
 ('Go to the gym', '2019-01-01 13:00:00', 2),
 ('Go to the supermarket', '2019-01-01 14:00:00', 3),
 ('Go to the cinema', '2019-01-01 15:00:00', 4),
 ('Go to the theatre', '2019-01-01 16:00:00', 5);
+*/
